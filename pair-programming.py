b5d0aa21f124ab0530b418d3357d031887629270
Eliza Ross <elizaross@mines.edu>
2024-02-26 12:10:54 -0700

    Add feedback and test funtion

--- pair-programming.py
+++ pair-programming.py
@@ -12,3 +12,21 @@ x, y = polar_to_cartesian(r, theta)
 x = round(x, 4)
 y = round(y, 4)
 print('Cartesian coordinates are: x =', x, 'and y =', y)
+
+
+# Feedback: Function works great. Variables have very clear names. Might want to add comments explaining what each line does
+
+#Test 1:
+
+r = 10
+theta = math.pi
+polar_to_cartesian(r, theta)
+print('Cartesian coordinates are: x =', x, 'and y =', y)
+
+#Test 2:
+
+r = 15
+theta = math.pi/6
+polar_to_cartesian(r, theta)
+print('Cartesian coordinates are: x =', x, 'and y =', y)
+
